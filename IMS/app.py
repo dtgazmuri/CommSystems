a77e88991eb48a91f6cc065c8fc2f7a1ef24d2ae
DIEGO GAZMURI S294224 <dtgazmuri@uc.cl>
2022-05-04 19:09:10 +0200

    Server updates

--- IMS/app.py
+++ IMS/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, request, Response
+from flask import Flask, render_template, request, Response
+from flaskext.mysql import MySQL
 import json
 
 app = Flask(__name__)
 
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'condorito'
+app.config['MYSQL_DATABASE_DB'] = 'LibrarySystem'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
 @app.route('/')
-def index():
-    return 'Hello world'
+def main():
+    return render_template('index.html')
+    # return 'Hello world'
 
 @app.route('/cakes')
 def cakes():
@@ -19,6 +31,41 @@ def RFID():
 
     #return Response(return_string, status=201)
     return return_string
+
+@app.route('/signin')
+def showSignIn():
+    return render_template('signin.html')
+
+@app.route('/api/signin', methods=["POST"])
+def signIn():
+    email = request.form["inputEmail"]   
+    password = request.form["inputPassword"]
+    print(email)
+    print(password)
+
+    if email and password:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM Users WHERE username=%s AND password=%s"
+        values = (email, password)
+
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        print(data)
+
+        if len(data) is 1:
+            print("Success")
+            user1 = str(data[0][3])
+            user2 = str(data[0][4])
+            user = user1 + " " + user2
+            print(user)
+            return json.dumps({'message': user})
+        else:
+            print("No")
+            return json.dumps({'message': 'Wrong credentials'})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
